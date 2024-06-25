1f9a91e751839d8d0dfb704c29b8810bc652c99d
ishnB <ishan.barpanda74@gmail.com>
2024-06-25 18:13:23 +0530

    minor fix

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ from sklearn.cluster import KMeans
 from textblob import TextBlob
 import openai
 import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
 openai.api_key = st.secrets['openai']["OPENAI_API_KEY"]
 
 
